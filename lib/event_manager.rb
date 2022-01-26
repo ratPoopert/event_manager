b18b7be2914a965f0c73838c20b768f46697c5db
ratPoopert <50669669+ratPoopert@users.noreply.github.com>
2022-01-26 19:45:20 +0000

    Handle good and bad zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -18,6 +18,11 @@ contents.each do |row|
   # if the zip code is exactly five digits, assume that it is okay.
   # if the zip code is more than five digits, truncate it to the first five digits.
   # if the zip code is less than five digits, add zeros to the front until it becomes five digits.
-  
+  if zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  end
+
   puts "#{name} #{zipcode}"
 end
