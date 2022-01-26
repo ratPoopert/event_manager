00e75f697c3a05997fea4433150a82737e62f080
ratPoopert <50669669+ratPoopert@users.noreply.github.com>
2022-01-26 21:11:56 +0000

    Fill template via gsub

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -37,5 +37,8 @@ contents.each do |row|
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  personal_letter = template_letter.gsub('FIRST_NAME', name)
+  personal_letter.gsub!('LEGISLATORS', legislators)
+
+  puts personal_letter
 end
