5aa2370415786acbd50a8286ed2ceaef489b6f50
ratPoopert <50669669+ratPoopert@users.noreply.github.com>
2022-01-26 19:43:18 +0000

    Add pseudocode for cleaning bad zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -14,5 +14,10 @@ contents.each do |row|
   # Access column via header name
   name = row[:first_name]
   zipcode = row[:zipcode]
+
+  # if the zip code is exactly five digits, assume that it is okay.
+  # if the zip code is more than five digits, truncate it to the first five digits.
+  # if the zip code is less than five digits, add zeros to the front until it becomes five digits.
+  
   puts "#{name} #{zipcode}"
 end
