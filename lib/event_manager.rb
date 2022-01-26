677c38860a99078140ebabf88253728e5af59617
ratPoopert <50669669+ratPoopert@users.noreply.github.com>
2022-01-26 18:14:26 +0000

    Display first name of all attendees

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,5 +2,11 @@ puts 'Event Manager Initialized!'
 
 lines = File.readlines('event_attendees.csv')
 lines.each do |line|
-  puts line
+
+  # Separate CSV columns
+  columns = line.split(",")
+
+  # Extract the name (third) column
+  name = columns[2]
+  puts name
 end
