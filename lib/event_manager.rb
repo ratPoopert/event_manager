a768c96ce997122a9e4d5df3cf7957b8e864b440
ratPoopert <50669669+ratPoopert@users.noreply.github.com>
2022-01-26 19:50:48 +0000

    Create clean_zipcode method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,30 +1,25 @@
-# Load the CSV library
 require 'csv'
+
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    '00000'
+  elsif zipcode.length < 5
+    zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  end
+end
+
 puts 'Event Manager Initialized!'
 
-# Load the CSV file using the CSV library.
 contents = CSV.open(
   'event_attendees.csv',
   headers: true,
-
-  # Convert headers to symbols
   header_converters: :symbol
 )
 contents.each do |row|
-  # Access column via header name
   name = row[:first_name]
-  zipcode = row[:zipcode]
-
-  # if the zip code is exactly five digits, assume that it is okay.
-  # if the zip code is more than five digits, truncate it to the first five digits.
-  # if the zip code is less than five digits, add zeros to the front until it becomes five digits.
-  if zipcode.nil?
-    zipcode = '00000'
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust(5, '0')
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  end
+  zipcode = clean_zipcode(row[:zipcode])
 
   puts "#{name} #{zipcode}"
 end
