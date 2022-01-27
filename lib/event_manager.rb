434bf9de9a2472704c444c9d4fadfcab87095212
ratPoopert <50669669+ratPoopert@users.noreply.github.com>
2022-01-27 13:33:11 +0000

    Implement #clean_phone_number

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -31,6 +31,24 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+def decorate_phone_number(phone_number)
+  area_code = phone_number[0..2]
+  exchange_number = phone_number[3..5]
+  subscriber_number = phone_number[6..9]
+  return area_code + "-" + exchange_number + "-" + subscriber_number
+end
+
+def clean_phone_number(phone_number)
+  clean_phone_number = phone_number.to_s.gsub(/[^0-9]/, '')
+  if clean_phone_number.length == 10
+    return decorate_phone_number(clean_phone_number)
+  elsif clean_phone_number.length == 11 and clean_phone_number[0] == "1"
+    return decorate_phone_number(clean_phone_number[1..])
+  else
+    return 'No phone number available.'
+  end
+end
+
 puts 'Event Manager Initialized!'
 
 contents = CSV.open(
@@ -46,9 +64,11 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  phone_number = clean_phone_number(row[:homephone])
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id, form_letter)
+  puts phone_number
 end
